93b12d1d86b7b30587a9110fe3f9f6ebb4ad1b1b
liangjunjie <792972988@qq.com>
2019-05-30 21:27:44 +0800

    总项目完成90%

--- src/components/reading/reading.vue
+++ src/components/reading/reading.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="reading-content" @touchstart.stop="appTouchStar" @touchend.stop="appTouchEnd" @click="flag=!flag">
-    <h4 id="top">{{chapterContent.title}}</h4>
-    <p v-for="(item,index) in textList" :key="index">
+  <div class="reading-content" @touchstart.stop="appTouchStar" @touchend.stop="appTouchEnd" @click="flag=!flag"
+       :style="{'background-color':backgroundColor}">
+    <h4 :style="{'font-size':titleSize+'vw'}">{{chapterContent.title}}</h4>
+    <p v-for="(item,index) in textList" :key="index" :style="{'font-size':fontSize+'vw'}">
       {{item}}
     </p>
     <!--头部-->
@@ -14,12 +15,34 @@
     <mu-drawer :open.sync="open" :docked="false" :right="true" style="background-color:rgb(115, 115, 115);">
       <mu-list>
         <div class="chapter" v-for="(item,index) in chapterList"
-             :class="{active:isActive(item.title)}"
-             :key="index" @click.stop="selectChapter(item.link)"
+             :class="{active:current==index}"
+             :key="index" @click.stop="selectChapter(item.link);active(index)"
         >{{item.title}}
         </div>
       </mu-list>
     </mu-drawer>
+
+    <!--设置框-->
+    <div class="footer" :class="{footerShow:flag}">
+      <mu-slider class="demo-slider" color="secondary" v-model="opacity" :max="70" :display-value="false"></mu-slider>
+      <div class="select-bgc" data-flex="cross:top">
+        <div data-flex-box="0" class="size" @click.stop="reduceFontSize">Aa-</div>
+        <div data-flex-box="0" class="bgc"
+             v-for="(item,index) in bgcList"
+             :class="{active:isBgc(item)}"
+             :style="{'background-color':item}"
+             @click.stop="backgroundColor=item"
+        ></div>
+        <div data-flex-box="0" class="size" @click.stop="addFontSize">Aa+</div>
+      </div>
+      <div class="foot" data-flex="cross:top">
+        <div data-flex-box="1" class="bt" @click.stop="reduceChapter()">上一章</div>
+        <div data-flex-box="1" class="bt ml" @click.stop="addChapter()">下一章</div>
+      </div>
+    </div>
+    <!--遮挡层-->
+
+    <div class="setOpa" :style="{'opacity':0.7-(opacity/100)}"></div>
   </div>
 </template>
 
@@ -28,6 +51,14 @@
     name: "reading",
     data() {
       return {
+        titleSize: 5,
+        fontSize: 4.5,
+        current: 0,
+        bgcList: [
+          '#E9E6D0', '#cce8cf', '#E9FAFF', '#E9EBFE', '#FDE6E0'
+        ],
+        backgroundColor: '#E9E6D0',
+        opacity: 55,
         open: false,
         flag: false,
         id: this.$route.query.id,
@@ -39,21 +70,61 @@
       }
     },
     methods: {
-      //选择章节
-      selectChapter(link){
-        this.flag=false
-        this.$indicator.open()
-        this.link=link;
-        this.getReadingContent()
-        window.scrollTo(0,0)
+      //上一章
+      reduceChapter() {
+        if (this.current <= 0) {
+          this.$toast({position: 'center', duration: 700, message: '没有上一章了'})
+        } else {
+          this.current -= 1
+          this.link = this.chapterList[this.current].link
+          this.getReadingContent()
+        }
       },
-      //是否加active
-      isActive(title) {
-        if (this.chapterContent.title == title) {
+      //下一章
+      addChapter() {
+        if (this.current >= this.chapterList.length - 1) {
+          this.$toast({position: 'center', duration: 700, message: '没有下一章了'})
+        } else {
+          this.current += 1
+          this.link = this.chapterList[this.current].link
+          this.getReadingContent()
+        }
+      },
+
+      // 字体增大
+      addFontSize() {
+        if (this.fontSize >= 6) {
+          this.$toast({position: 'center', duration: 700, message: '不能再大了哦'})
+        } else {
+          this.titleSize += 0.2
+          this.fontSize += 0.2
+        }
+      },
+      //字体减小
+      reduceFontSize() {
+        if (this.fontSize <= 3.5) {
+          this.$toast({position: 'center', duration: 700, message: '不能再小了哦'})
+        } else {
+          this.titleSize -= 0.2
+          this.fontSize -= 0.2
+        }
+      },
+      isBgc(item) {
+        if (this.backgroundColor == item) {
           return true
         }
       },
-      //什么事都不做，取消冒泡，谁需要谁调用
+      //选择章节
+      selectChapter(link) {
+        this.link = link;
+        this.getReadingContent()
+        window.scrollTo(0, 0)
+      },
+      //是否加active
+      active(index) {
+        this.current = index
+      },
+      //什么事都不做，取消冒泡，如果没有点击事件加这个上去
       nothing() {
       },
       goBack() {
@@ -88,6 +159,7 @@
       },
       //获取章节内容
       getReadingContent() {
+        this.$indicator.open()
         this.$http.get('/chapter/chapter/' + this.link).then(result => {
           if (result.statusText == 'OK') {
             this.chapterContent = result.data.chapter
@@ -97,6 +169,7 @@
           }
           this.$indicator.close()
         })
+        console.log(this.textList[0])
       }
     },
     created() {
@@ -104,21 +177,7 @@
       this.getTrue()
     },
     mounted() {
-      // if (this.$route.query.bookListType.indexOf('recommend') != -1) {
-      //   this.bookListType = 'recommend'
-      //   this.$route.meta.index = 2
-      // } else if (this.$route.query.bookListType.indexOf('home') != -1) {
-      //   this.bookListType = 'home'
-      //   this.$route.meta.index = 1
-      // } else if (this.$route.query.bookListType.indexOf('classification') != -1) {
-      //   this.bookListType = 'classification'
-      //   this.$route.meta.index = 3
-      // } else if (this.$route.query.bookListType.indexOf('search') != -1) {
-      //   this.bookListType = 'search'
-      //   this.$route.meta.index = 4
-      // } else {
-      //
-      // }
+
     },
 
   }
@@ -126,50 +185,131 @@
 
 <style lang="less" scoped>
   .reading-content {
+    transition: all 0.4s;
+    .mu-paper {
+      width: 50vw;
+    }
     .chapter {
       text-overflow: ellipsis;
       white-space: nowrap;
       overflow: hidden;
       text-align: center;
-      color: #4a4a4a;
-      font-size: 18px;
-      line-height: 50px;
+      color: #3d3d3d;
+      font-size: 3.5vw;
+      line-height: 12.5vw;
     }
     .active {
       background-color: #999999;
     }
     .mui-bar-nav {
+      box-shadow: none;
+      z-index: 3001;
       background-color: rgb(115, 115, 115);
       transition: all 0.4s;
       height: 0;
       opacity: 0;
       h1 {
+        color: #343434;
         font-size: 4.5vw;
         line-height: 11vw;
       }
       .mui-icon {
         font-size: 6.25vw;
         line-height: 5vw;
+        color: #4a4a4a;
       }
     }
     .showBar {
-      height: 11vw;
+      height: 10vw;
       opacity: 1;
     }
     min-height: 100vh;
     padding: 2.4vw;
-    background-color: rgba(204, 232, 207, 1);
     p {
-      color: #4a4a4a;
-      font-size: 4.5vw;
+      color: #755927;
       line-height: 7.5vw;
     }
     h4 {
+      color: #4a4a4a;
       line-height: 11.25vw;
-      margin-top: 2.5vw;
+      margin-top: 6vw;
       width: 100%;
       text-align: center;
-      font-size: 4.5vw;
+      font-size: 5vw;
+    }
+    .footer {
+      z-index: 3001;
+      opacity: 0;
+      transition: all 0.4s;
+      background-color: rgb(115, 115, 115);
+      height: 0;
+      position: fixed;
+      width: 100vw;
+      bottom: 0;
+      left: 0;
+      .demo-slider {
+        margin: auto;
+        margin-top: 5vw;
+        width: 65vw;
+        background: transparent !important;
+      }
+      .select-bgc {
+        width: 90vw;
+        margin: 0 auto;
+        margin-top: 5vw;
+        .bgc {
+          transition: all 0.4s;
+          margin-left: 4vw;
+          width: 10vw;
+          height: 10vw;
+          border-radius: 50%;
+          box-sizing: border-box;
+        }
+        .bgc:nth-child(2) {
+          margin-left: 3vw;
+        }
+        .size {
+          margin-left: 3vw;
+          font-size: 5vw;
+          line-height: 10vw;
+          color: mediumpurple;
+        }
+        .size:nth-child(1) {
+          margin-left: 0;
+        }
+        .active {
+          border-radius: 3vw;
+        }
+      }
+      .foot {
+        margin: auto;
+        margin-top: 7vw;
+        width: 65vw;
+        .bt {
+          font-size: 4.5vw;
+          color: #4a4a4a;
+          background-color: mediumpurple;
+          text-align: center;
+          border-radius: 6px;
+          line-height: 10vw;
+        }
+        .ml {
+          margin-left: 2.5vw;
+        }
+      }
+    }
+    .footerShow {
+      height: 50vw;
+      opacity: 1;
+    }
+    .setOpa {
+      background-color: #000000;
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      opacity: 0;
     }
   }
 </style>
